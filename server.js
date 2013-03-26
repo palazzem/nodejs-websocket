03de0feeffe6a98b0519757688d8dc5b49c6fe6d
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2013-03-26 23:41:43 +0100

    Webserver push timestamp data

--- server.js
+++ server.js
@@ -36,16 +36,12 @@ wsServer.on('request', function(request) {
 
     var connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
+
+    // It's just a test :)
+    while(true) {
+        connection.sendUTF(new Date());
+    }
+
     connection.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
